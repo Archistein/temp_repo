290d33b74e3284b80aeac4178e888ffb6e62b77c
Archistein <turods@yandex.ru>
2025-12-05 00:06:21 +0300

    Add more asserts

--- test_app.py
+++ test_app.py
@@ -1,10 +1,22 @@
-import pytest
+import math
 from fastapi.testclient import TestClient
 from app import app
 
 client = TestClient(app)
 
-def test_read_main():
+def test_calc():
     response = client.post("/api/v1/calc", params={'a': 1, 'op': '+', 'b': 2})
     assert response.status_code == 200
     assert response.json() == 3
+
+    response = client.post("/api/v1/calc", params={'a': 8.7, 'op': '-', 'b': 3.4})
+    assert response.status_code == 200
+    assert math.isclose(response.json(), 5.3)
+
+    response = client.post("/api/v1/calc", params={'a': 7, 'op': '*', 'b': 7})
+    assert response.status_code == 200
+    assert response.json() == 49
+
+    response = client.post("/api/v1/calc", params={'a': 5, 'op': '/', 'b': 2})
+    assert response.status_code == 200
+    assert response.json() == 2.5
